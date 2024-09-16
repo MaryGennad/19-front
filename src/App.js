c0b875087e798f3f0c5292df5a792aef89b3443b
MarryGennad <maria-gennadievna@outlook.com>
2024-09-16 13:19:55 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [product, setProduct] = useState({})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>¯\_(ツ)_/¯ Lesson 19 Client (Front-end)</h1>
+      <hr></hr>
+      <button onClick={async () => {
+        let res = await fetch('http://localhost:2000/api/product')
+        let resJson = await res.json()
+        setProduct(resJson)
+      }}>Загрузить</button>
+      <h2>Блюдо: {product.name}</h2>
+    </>
   );
 }
 
